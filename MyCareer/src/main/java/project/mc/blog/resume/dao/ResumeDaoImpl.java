41913d616432b1558e3f306fa9d0406f5171f56b
GangNamgu <namgu333@naver.com>
2017-10-10 09:39:50 +0900

    ㅓ

--- MyCareer/src/main/java/project/mc/blog/resume/dao/ResumeDaoImpl.java
+++ MyCareer/src/main/java/project/mc/blog/resume/dao/ResumeDaoImpl.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 import project.mc.blog.resume.domain.ResumeVO;
+import project.mc.blog.user.domain.UserVO;
 import project.mc.commons.DTO;
 
 
@@ -113,6 +114,7 @@ public class ResumeDaoImpl implements ResumeDao {
 		
 		return sqlSession.selectOne(statement, inResumeVO);
 	}
+
 
 
 
